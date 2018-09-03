b5b42deab27feedd19a0791b9e258af1516a17a0
HaTien <havantien95tb@gmail.com>
2018-09-03 12:38:21 +0700

    class application

---
+++ src/main/java/fan/Application.java
@@ -0,0 +1,18 @@
+package fan;
+
+public class Application {
+    public static void main(String[] args) {
+        Fan fan_1 = new Fan();
+        fan_1.setColor("3");
+        fan_1.setRadius(10);
+        fan_1.setColor("yellow");
+        fan_1.setOn(true);
+        System.out.println(fan_1.toString());
+
+        Fan fan_2 = new Fan();
+        fan_2.setSpeed(2);
+        fan_2.setRadius(5);
+        System.out.println(fan_2.toString());
+
+    }
+}
